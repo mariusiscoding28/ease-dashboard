853ee30567015ff796bfee15aa4e22e2b9df39c8
mariusiscoding28 <122819193+mariusiscoding28@users.noreply.github.com>
2023-01-24 17:19:27 -0500

    Added dropdown menu component

---
+++ video-1/finished/js/main.js
@@ -0,0 +1,37 @@
+const dropdown = () => {
+  const _variables = {
+    main: "e-dropdown",
+    target: "data-dropdown-target",
+    active: "e-active",
+  }
+
+  const dropDown = [...document.querySelectorAll(`.${_variables.main}`)]
+
+  document.addEventListener("click", (e) => {
+
+    const target = e.target.closest(`.${_variables.main}`)
+
+    if (!target) return
+
+    const targetId = target.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+
+    const activeMenu = document.querySelector(`#${targetId}`)
+
+    const nonTargeted = dropDown.map(drop => {
+      const nonActiveId = drop.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+      const nonActive = document.querySelector(`#${nonActiveId}`)
+
+      return nonActive
+    })
+
+    const filterExceptActive = nonTargeted.filter(target => target !== activeMenu)
+
+    filterExceptActive.forEach(drop => drop.classList.remove(_variables.active))
+
+    if (activeMenu) activeMenu.classList.toggle(_variables.active)
+
+  })
+
+}
+
+dropdown()
